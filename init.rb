7fc7102d4d996f75442055d3e2f6e06232cdc419
Victor Brender <victor.brender@gmail.com>
2016-11-14 12:03:22 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :project_author do
+  name 'Project Author plugin'
+  author 'Brender Victor'
+  description 'This is a plugin for adding the project author from a list of already registered users.'
+  version '0.0.1'
+end
